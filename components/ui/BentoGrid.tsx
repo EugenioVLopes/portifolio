312b3d827879e8cec73c17f56ccbe763cd997622
EUGENIO SANTOS <eugenio.lopes.090@ufrn.edu.br>
2024-06-29 16:08:02 -0300

    alterando nome de botao

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -77,19 +77,19 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        // remove p-4 rounded-3xl dark:bg-black dark:border-white/[0.2] bg-white  border border-transparent, add border border-white/[0.1] overflow-hidden relative
+        // remover p-4 rounded-3xl dark:bg-black dark:border-white/[0.2] bg-white  border border-transparent, adicionar border border-white/[0.1] overflow-hidden relative
         "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
         className
       )}
       style={{
-        //   add these two
-        //   you can generate the color from here https://cssgradient.io/
+        // adicionar essas duas propriedades
+        // você pode gerar a cor a partir daqui https://cssgradient.io/
         background: "rgb(4,7,29)",
         backgroundColor:
           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
-      {/* add img divs */}
+      {/* adicionar divs para as imagens */}
       <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
@@ -114,7 +114,7 @@ export const BentoGridItem = ({
           )}
         </div>
         {id === 6 && (
-          // add background animation , remove the p tag
+          // adicionar animação de fundo, remover a tag p
           <BackgroundGradientAnimation>
             <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
           </BackgroundGradientAnimation>
@@ -126,25 +126,25 @@ export const BentoGridItem = ({
             "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
           )}
         >
-          {/* change the order of the title and des, font-extralight, remove text-xs text-neutral-600 dark:text-neutral-300 , change the text-color */}
+          {/* alterar a ordem do título e descrição, font-extralight, remover text-xs text-neutral-600 dark:text-neutral-300, alterar a cor do texto */}
           <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
             {description}
           </div>
-          {/* add text-3xl max-w-96 , remove text-neutral-600 dark:text-neutral-300*/}
-          {/* remove mb-2 mt-2 */}
+          {/* adicionar text-3xl max-w-96, remover text-neutral-600 dark:text-neutral-300 */}
+          {/* remover mb-2 mt-2 */}
           <div
             className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}
           >
             {title}
           </div>
 
-          {/* for the github 3d globe */}
+          {/* para o globo 3D do GitHub */}
           {id === 2 && <GridGlobe />}
 
-          {/* Tech stack list div */}
+          {/* Div para a lista de tecnologias */}
           {id === 3 && (
             <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
-              {/* tech stack lists */}
+              {/* listas de tecnologias */}
               <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
                 {leftLists.map((item, i) => (
                   <span
@@ -173,10 +173,10 @@ export const BentoGridItem = ({
           )}
           {id === 6 && (
             <div className="mt-5 relative">
-              {/* button border magic from tailwind css buttons  */}
-              {/* add rounded-md h-8 md:h-8, remove rounded-full */}
-              {/* remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 */}
-              {/* add handleCopy() for the copy the text */}
+              {/* botão com borda mágica do Tailwind CSS */}
+              {/* adicionar rounded-md h-8 md:h-8, remover rounded-full */}
+              {/* remover focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 */}
+              {/* adicionar handleCopy() para copiar o texto */}
               <div
                 className={`absolute -bottom-5 right-0 ${copied ? "block" : "block"
                   }`}
@@ -186,7 +186,7 @@ export const BentoGridItem = ({
               </div>
 
               <MagicButton
-                title={copied ? "Email is Copied!" : "Copy my email address"}
+                title={copied ? "Email copiado!" : "Copiar meu endereço de e-mail"}
                 icon={<IoCopyOutline />}
                 position="left"
                 handleClick={handleCopy}
